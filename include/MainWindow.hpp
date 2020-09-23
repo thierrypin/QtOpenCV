362ae900271bc135fbd2585e639a6caffc00a504
Thierry <thierrypin@gmail.com>
2020-09-23 19:26:48 -0300

    limpeza de um add acidental

---
+++ include/MainWindow.hpp
@@ -0,0 +1,15 @@
+//
+// Created by thierry on 23/09/2020.
+//
+
+#ifndef QTOPENCV_QTMAINWINDOW_HPP
+#define QTOPENCV_QTMAINWINDOW_HPP
+
+
+class QtMainWindow
+{
+
+};
+
+
+#endif //QTOPENCV_QTMAINWINDOW_HPP
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,5 +2,4 @@
 // Created by thierry on 23/09/2020.
 //
 
-#include "SampleClass.hpp"
-
+#include "MainWindow.hpp"
