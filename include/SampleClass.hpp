ee8613f2767b89dbfe591afc77b20dffe3b9e2f6
Thierry <thierrypin@gmail.com>
2020-09-23 19:01:03 -0300

    sampleclass

---
+++ include/SampleClass.hpp
@@ -0,0 +1,15 @@
+//
+// Created by thierry on 23/09/2020.
+//
+
+#ifndef QTOPENCV_SAMPLECLASS_HPP
+#define QTOPENCV_SAMPLECLASS_HPP
+
+
+class SampleClass
+{
+
+};
+
+
+#endif //QTOPENCV_SAMPLECLASS_HPP
---
+++ src/SampleClass.cpp
@@ -0,0 +1,6 @@
+//
+// Created by thierry on 23/09/2020.
+//
+
+#include "SampleClass.hpp"
+
