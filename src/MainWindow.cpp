78dccf1cd3a7089a3079658ed229e758de7079c7
gabrielclow <gabrielclow@users.noreply.github.com>
2020-09-23 21:44:41 -0300

    Layout main window

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -26,6 +26,51 @@ void MainWindow::draw()
     window = make_shared<QWidget>();
     layout = make_shared<QHBoxLayout>();
 
+    /*
+     * Lado esquerdo da tela
+     */
+    vbox_left = make_shared<QVBoxLayout>();
+
+    // Label dados usuario
+    vbox_left->addWidget(lbl_dados_usuario.get());
+
+    // HBox nome
+    hbox_vbox_left_1 = make_shared<QVBoxLayout>();
+    hbox_vbox_left_1->addWidget(lbl_nome.get());
+    hbox_vbox_left_1->addWidget(nome.get());
+
+    vbox_left->addLayout(hbox_vbox_left_1.get());
+
+    // HBox matricula
+    hbox_vbox_left_2 = make_shared<QVBoxLayout>();
+    hbox_vbox_left_2->addWidget(lbl_matricula.get());
+    hbox_vbox_left_2->addWidget(matricula.get());
+
+    vbox_left->addLayout(hbox_vbox_left_2.get());
+
+    // HBox rostos
+    hbox_vbox_left_3 = make_shared<QVBoxLayout>();
+    hbox_vbox_left_3->addWidget(img_face.get());
+    //hbox_vbox_left_3->addWidget(PLACEHOLDER.get());
+
+    vbox_left->addLayout(hbox_vbox_left_3.get());
+
+    // Status acesso
+    vbox_left->addLayout(status_acesso.get());
+
+    // Data
+    vbox_left->addLayout(data.get());
+
+    /*
+     * Lado direito da tela
+     */
+    vbox_right = make_shared<QVBoxLayout>();
+
+    // Label posicionamento
+    vbox_right->addWidget(lbl_posicionamento.get());
+
+    // Imagem da camera
+    vbox_left->addWidget(img.get());
 
     // Ligação dos elementos ao layout
 //    layout->addWidget(button1.get());
